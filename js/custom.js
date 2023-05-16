fa254fb2fcdabb746d0f1e0154996968a21d4eb5
jaehan-Bae <yanggo31@gmail.com>
2023-05-16 20:21:26 +0900

    first commit

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$(function() {
+});
+
+// Main Slide
+// function slideRight() {
+//   var slideContainer = document.querySelector('.slide-container');
+//   var slideWidth = document.querySelector('.main-con').offsetWidth;
+//   slideContainer.style.transform = 'translateX(-' + slideWidth + 'px)';
+//   setTimeout(function() {
+//     slideContainer.appendChild(slideContainer.firstElementChild);
+//     slideContainer.style.transition = 'none';
+//     slideContainer.style.transform = 'translateX(0)';
+//     setTimeout(function() {
+//       slideContainer.style.transition = 'transform 1s ease-in-out';
+//     },10);
+//   }, 1000);
+//   }setInterval(slideRight, 3000);
+
